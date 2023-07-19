73e5f56553e9c70014edbbb76400f775720ac67c
TEMHITHORPHE <Tehmhithorphe@gmail.com>
2023-07-19 23:09:11 +0100

    wow ... content-type error

--- signup.js
+++ signup.js
@@ -6,13 +6,22 @@
 // https://spotplus.com/Api/Mobile/emailregister
 //  https://spotplus.com/Api/Mobile/emailcode?email=test@gmail.com
 
-let FORM_INPUT = {}
-
 // setTimeout(()=> {
 // 	ShowErrorAlert("Testing Me Out");
 // 	ShowSuccessAlert("Testing Me Out");
 
-// }, 1000);
+// }, 50000);
+
+
+
+let FORM_INPUT = {}
+
+
+const CREATE_ACCOUNT_ENDPOINT = "https://spotplus.com/Api/Mobile/emailregister";
+const CREATE_ACCOUNT_ERROR_MOCK_ENDPOINT = "http://localhost:5500/invalid_register.json";
+const CREATE_ACCOUNT_SUCCESS_MOCK_ENDPOINT = "http://localhost:5500/success_register.json";
+const EMAIL_OTP_ENDPOINT = "https://spotplus.com/Api/Mobile/emailcode?email=";
+
 
 
 async function OnEmailVerify(e) {
@@ -22,6 +31,7 @@ async function OnEmailVerify(e) {
 
 	const emailOTP = form.email_codebox.value;
 	console.log(emailOTP);
+
 	if (emailOTP.length != 6 || Number(emailOTP) == NaN) {
 		ShowErrorAlert("email verification code invalid");
 		return;
@@ -32,21 +42,28 @@ async function OnEmailVerify(e) {
 		verify: Number(emailOTP)
 	});
 
-	const res = await fetch("https://spotplus.com/Api/Mobile/emailregister", {
+	const formData = (new FormData());
+	formData.set('username',NAME)
+    formData.set('email',EMAIL)
+    formData.set('password',PASSWORD)
+    formData.set('verify',EMAIL_OTP_CODE)
+    formData.set('invit',INVITE_CODE);
+
+	const res = await fetch(CREATE_ACCOUNT_ENDPOINT, {
 		method: "POST",
 		mode: "cors",
-		body: JSON.stringify({
-			...FORM_INPUT,
-			verify: Number(emailOTP)
-		}),
-
+		headers: "application/x-www-form-urlencoded",
+		body: formData
 	});
 
 
 	if (res.status === 200) {
 
-		let data = await res.json();
-		data = Number(data.status)
+		// let data = await res.text; // For Testing!!
+		let data = await res.text();
+		// data = Number(data.status)
+
+		console.log(data);
 
 		if (data != NaN && data.status === 1) {
 			// TODO: POP AN ALERT TO USER
@@ -112,7 +129,7 @@ async function OnRegister__ClickHandler(e, r) {
 
 	// console.log("FORM_INPUT: ", FORM_INPUT);
 
-	const res = await fetch("https://spotplus.com/Api/Mobile/emailcode?email=" + email, {
+	const res = await fetch(EMAIL_OTP_ENDPOINT + email, {
 		method: "POST",
 		mode: "cors"
 	});
@@ -165,6 +182,8 @@ function updateFormPage(page) {
 
 function ShowErrorAlert(message) {
 	const alert = document.getElementById("alert-section");
+
+	// Possible (self) Cross Site Scripting Here ... I don't like it too.
 	const alertContent = `
 	<svg style="width: 24px; height: 24px" xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
 	<path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
@@ -191,6 +210,8 @@ function ShowErrorAlert(message) {
 
 function ShowSuccessAlert(message) {
 	const alert = document.getElementById("alert-section");
+
+	// Possible (self) Cross Site Scripting Here ... I don't like it too.
 	const alertContent = `
 	<svg style="width: 24px; height: 24px" xmlns="http://www.w3.org/2000/svg" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
 		<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z">
